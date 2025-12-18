baac1d82dd300380272494bf11fe13616881e2a2
thanday <azman.abdulla@gmail.com>
2025-12-19 01:34:28 +0500

    pm2 config update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,20 +2,17 @@ module.exports = {
     apps: [
       {
         name: "volleyball-server",
-        script: "./server.js",
+        script: "server.js",
         env: {
           NODE_ENV: "production",
         },
-        // Restarts if it uses too much memory
         max_memory_restart: "1G",
-        // Watch for changes in the server file to restart automatically
-        watch: ["./server/server.js"],
+        watch: ["server.js"],
       },
       {
         name: "volleyball-client",
         script: "npm",
-        args: "run dev",
-        cwd: "./volleyball-broadcast", 
+        args: "run dev -- --host",
         env: {
           NODE_ENV: "development",
         },
